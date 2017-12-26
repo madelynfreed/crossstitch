191aeae59a26ea64d6c75e18e7fdecb3b249c157
Madelyn Freed <madelyn.freed@gmail.com>
2017-12-26 13:11:20 -0500

    basic working django skeleton, no views yet

--- crossstitch/crossstitch/crossstitch/urls.py
+++ crossstitch/crossstitch/crossstitch/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    url(r'^admin/', admin.site.urls),
+    url(r'^uploadimage/', include('uploadimage.urls')),
+    url(r'^$', RedirectView.as_view(url='/uploadimage', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
--- crossstitch/crossstitch/upload_image/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UploadImageConfig(AppConfig):
-    name = 'upload_image'
